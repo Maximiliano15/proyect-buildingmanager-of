ed02091333ede5abeee40489629176b811859cd5
Maximiliano <Maximiliano@Maximiliano-PC>
2016-04-19 17:27:28 -0400

    visualizar los datos con el monto  a pagar(forma muy primitiva)

--- BuildingManager/src/buildingmanager/BuildingManager.java
+++ BuildingManager/src/buildingmanager/BuildingManager.java
@@ -5,6 +5,8 @@
  */
 package buildingmanager;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Maximiliano
@@ -15,7 +17,39 @@ public class BuildingManager {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int bodega=20;
+        int cale=2000;
+        int estac=5000;
+        int gas=3000;
+        mostrarGrastos(gas,bodega,cale,estac);
+    }
+
+    public static String nombreCopropietario() {
+        String nombre = JOptionPane.showInputDialog("Nombre del co´propietario");
+        return nombre;
+    }
+
+    public static String apellidoCopropietario() {
+        String apellido = JOptionPane.showInputDialog("Apellido del co´propietario");
+        return apellido;
+    }
+
+    public static int numDepartamento() {
+        int numdepto = Integer.parseInt(JOptionPane.showInputDialog("N° depto del copropietario"));
+        return numdepto;
     }
     
+    public static int montopagar(int gastosC,int bodega, int cale, int estac){
+        
+        int total=gastosC+bodega+cale+estac;
+        return total;
+    }
+    
+    
+
+    public static void mostrarGrastos(int gasto_com, int bodeg, int estacionamient, int calef) {
+        JOptionPane.showMessageDialog(null, "Nombres del copropietario | Apellido del copropietario | Depto | Gasto Comun | Bodega | Calefaccion | Estacionamiento | Total a pagar\n"
+                + nombreCopropietario() + "   " + apellidoCopropietario() + "   " + numDepartamento() + "  " + gasto_com + "   " + bodeg + "  " + estacionamient + "  "+montopagar(gasto_com, bodeg,calef,estacionamient));
+    }
+
 }
