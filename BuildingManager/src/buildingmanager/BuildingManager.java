93909853a61662d1835e51851323432e64276990
Maximiliano <Maximiliano@Maximiliano-PC>
2016-04-26 00:14:39 -0400

    Una version mas avanzada.. estan los metodos-----Falta actualizar deuda, y que no actualice solo el primer copro, ,,,, se uso una matriz

--- BuildingManager/src/buildingmanager/BuildingManager.java
+++ BuildingManager/src/buildingmanager/BuildingManager.java
@@ -6,6 +6,7 @@
 package buildingmanager;
 
 import javax.swing.JOptionPane;
+import sun.awt.SunHints;
 
 /**
  *
@@ -17,34 +18,85 @@ public class BuildingManager {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        menu();
+
+    }
+
+    public static void menu() {
+        String[][] copropietario = new String[10][8];
+
+        int op = 0;
         int bodega = 20;
         int cale = 2000;
         int estac = 5000;
         int gas = 3000;
+        int a = 0;
 
-        mostrarGrastos(gas, bodega, cale, estac, pagar(montopagar(gas, bodega, cale, estac), dineroCopr()));
+        do {
 
+            try {
+                op = Integer.parseInt(JOptionPane.showInputDialog("1. Gasto del los copropietarios\n 2. Pagar\n 3. mostrar\n 4. Actualizar"));
+                switch (op) {
+                    case 1: {
+
+                        ingresar(a, copropietario, gas, bodega, cale, estac, montopagar(gas, bodega, cale, estac));//momto actualizado);
+                        a++;
+                        break;
+                    }
+                    case 2: {
+
+                        JOptionPane.showMessageDialog(null, pagar(montopagar(gas, bodega, cale, estac), dineroCopr()));
+                        break;
+                    }
+                    case 3: {
+                        mostrar(copropietario);
+                       
+                        break;
+                    }
+                    case 4: {
+                           ActualizarDatos(copropietario);
+                        break;
+                    }
+                    default: {
+                        JOptionPane.showMessageDialog(null, "opcion invalida");
+                        break;
+                    }
+                }
+
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "invalida su opcion");
+            }
+        } while (op <= 7);
     }
 
     public static String nombreCopropietario() {
-        String nombre = JOptionPane.showInputDialog("Nombre del co´propietario");
+        String nomb = "";
+        do {
+            nomb = JOptionPane.showInputDialog("Nombre del co´propietario");
+        } while ("".equals(nomb));
 
-        return nombre;
+        return nomb;
     }
 
     public static String apellidoCopropietario() {
-        String apellido = JOptionPane.showInputDialog("Apellido del co´propietario");
-
+        String apellido = "";
+        do {
+            apellido = JOptionPane.showInputDialog("Apellido del co´propietario");
+        } while ("".equals(apellido));
         return apellido;
     }
 
     public static int numDepartamento() {
         int numdepto = 0;
-        try {
-            numdepto = Integer.parseInt(JOptionPane.showInputDialog("N° depto del copropietario"));
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "error");
-        }
+
+        do {
+            try {
+                numdepto = Integer.parseInt(JOptionPane.showInputDialog("N° depto del copropietario"));
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "error");
+            }
+        } while (numdepto <= 0);
         return numdepto;
     }
 
@@ -85,9 +137,64 @@ public class BuildingManager {
         return dinero;
     }
 
-    public static void mostrarGrastos(int gasto_com, int bodeg, int estacionamient, int calef, int monto) {
-        JOptionPane.showMessageDialog(null, "Nombres del copropietario | Apellido del copropietario | Depto | Gasto Comun | Bodega | Calefaccion | Estacionamiento | Total a pagar\n"
-                + nombreCopropietario() + "   " + apellidoCopropietario() + "   " + numDepartamento() + "  " + gasto_com + "   " + bodeg + "  " + estacionamient + "  " + monto);
+    public static void ingresar(int s, String[][] arreglo, int gasto_com, int bodeg, int estacionamient, int calef, int monto) {
+
+        arreglo[s][0] = nombreCopropietario();
+        arreglo[s][1] = apellidoCopropietario();
+        arreglo[s][2] = "" + numDepartamento();
+        arreglo[s][3] = "" + gasto_com;
+        arreglo[s][4] = "" + bodeg;
+        arreglo[s][5] = "" + estacionamient;
+        arreglo[s][6] = "" + calef;
+        arreglo[s][7] = "" + montopagar(gasto_com, bodeg, calef, estacionamient);
+
+        for (int i = 0; i < arreglo.length; i++) {
+
+            for (int j = 0; j < arreglo[i].length; j++) {
+
+            }
+            System.out.println();
+
+        }
+        System.out.println(" ");
+
+    }
+
+    public static void mostrar(String[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+
+                System.out.print(array[i][j] + " ");
+
+            }
+            System.out.println();
+
+        }
+
+    }
+
+    public static String[][] ActualizarDatos(String[][] Arra) {
+        int op = 0;
+        
+        String nombreNuevo = nombreCopropietario();
+        String ApellidoNuevo = apellidoCopropietario();
+        String n = "" + numDepartamento();
+        for (int i = 0; i < Arra.length; i++) {
+            for (int j = 0; j < Arra[i].length; j++) {
+                
+                if (Arra[i][0].equals(nombreNuevo) && Arra[i][1].equals(ApellidoNuevo) && Arra[i][2].equals(n));
+            }
+                JOptionPane.showMessageDialog(null, "Exite el copropietario. Nombre del nuevo copropietario");
+
+                Arra[i][0] = nombreCopropietario();
+                Arra[i][1] = apellidoCopropietario();  //cont
+                Arra[i][2] = "" + numDepartamento();
+                break;
+            
+
+        }
+
+        return Arra;
     }
 
 }
